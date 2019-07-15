c8a20a2908bf3d8dd457c57500b9e2c2812164ae
buckhx <buckhx@gmail.com>
2019-07-15 12:04:00 -0400

    Implement unicode checks

--- tokenization/unicode.go
+++ tokenization/unicode.go
@@ -5,7 +5,18 @@ package tokenization
 
 import "unicode"
 
-// IsWhitespace checks whether rune c is a whitespace character
+// _Bp is the BERT extension of the Punctuation character range
+var _Bp = &unicode.RangeTable{
+	R16: []unicode.Range16{
+		{0x0021, 0x002f, 1}, // 33-47
+		{0x003a, 0x0040, 1}, // 58-64
+		{0x005b, 0x0060, 1}, // 91-96
+		{0x007b, 0x007e, 1}, // 123-126
+	},
+	LatinOffset: 4, // All less than 0x00FF
+}
+
+// IsWhitespace checks whether rune c is a BERT whitespace character
 func IsWhitespace(c rune) bool {
 	switch c {
 	case ' ':
@@ -20,12 +31,20 @@ func IsWhitespace(c rune) bool {
 	return unicode.Is(unicode.Zs, c)
 }
 
-// IsControl checks wher rune c is a control character
+// IsControl checks wher rune c is a BERT control character
 func IsControl(c rune) bool {
-	return false
-
+	switch c {
+	case '\t':
+		return false
+	case '\n':
+		return false
+	case '\r':
+		return false
+	}
+	return unicode.In(c, unicode.Cc, unicode.Cf)
 }
 
+// IsPunctuation checks wher rune c is a BERT punctuation character
 func IsPunctuation(c rune) bool {
-	return false
+	return unicode.In(c, _Bp, unicode.P)
 }
